f2e2e74a0c175963d2ebf4684986213a2ed37973
kimsy701 <kimsy701@naver.com>
2024-08-08 09:18:00 +0900

    Create slice_to_half_height.py

---
+++ etc/slice_to_half_height.py
@@ -0,0 +1,38 @@
+import cv2
+import os
+
+# 이미지가 있는 폴더 경로
+folder_path = 'C:/Users/인쇼츠/Desktop/Deinterlacing구현/inference_data/메디컬아일랜드/MI원본'
+
+# 폴더 내 모든 이미지 파일 순회
+for img_idx, filename in enumerate(sorted(os.listdir(folder_path))):
+    image_path = os.path.join(folder_path, filename)
+    print(image_path)
+    image = cv2.imread(image_path)
+    
+    if image is None:
+        print(f"Failed to read {filename}. Skipping...")
+        continue
+
+    print(f"Processing {filename} with shape {image.shape}")
+
+    processed_frame1 = image[::2, :, :]
+    processed_frame2 = image[1::2, :, :]
+
+    # BGR 이미지를 RGB로 변환 (Matplotlib에서 제대로 보이도록)
+    processed_frame1_rgb = cv2.cvtColor(processed_frame1, cv2.COLOR_BGR2RGB)
+    processed_frame2_rgb = cv2.cvtColor(processed_frame2, cv2.COLOR_BGR2RGB)
+
+    # 결과 이미지 파일로 저장
+    output_folder = 'C:/Users/인쇼츠/Desktop/Deinterlacing구현/inference_data/메디컬아일랜드/MI_처리된'
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    frame1_path = os.path.join(output_folder, f'processed_frame_{img_idx}_1.png')
+    frame2_path = os.path.join(output_folder, f'processed_frame_{img_idx}_2.png')
+    cv2.imwrite(frame1_path, cv2.cvtColor(processed_frame1_rgb, cv2.COLOR_RGB2BGR))
+    cv2.imwrite(frame2_path, cv2.cvtColor(processed_frame2_rgb, cv2.COLOR_RGB2BGR))
+
+    print(f"Saved processed frames as {frame1_path} and {frame2_path}")
+
+print("Processing complete.")
